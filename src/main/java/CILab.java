98d83e8bd69111d782d1754d864d47cda363eb66
Gihane Ndjeuha <gihanen.ndjeuha@bellevuecollege.edu>
2021-06-13 12:13:39 -0700

    Update CILab and CILabTest files

--- src/main/java/CILab.java
+++ src/main/java/CILab.java
@@ -3,17 +3,30 @@ public class CILab implements CILabInterface {
 
     @Override
     public String getString() {
+
         return myString;
     }
 
     @Override
     public void setString(String string) {
+
         myString = string;
     }
 
     @Override
     public boolean detectCapitalUse() {
-        return false;
+        String word  = getString();
+        int caps=0;
+        for(int i=0; i<word.length(); i++)
+        {
+            if(Character.isUpperCase(word.charAt(i)))
+            {
+                caps++;
+
+            }
+        }
+        if(caps==word.length() || caps==0) return true;
+        return caps == 1 && Character.isUpperCase(word.charAt(0));
     }
 
 }
